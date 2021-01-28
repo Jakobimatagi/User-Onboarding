ca1f56a5c2592b30748df12d384055eeb36271f5
Jakobi Matagi <jkmatagi@hotmail.com>
2021-01-27 19:01:00 -0700

    mvp done

--- advanced/src/Components/Form.js
+++ advanced/src/Components/Form.js
@@ -1,74 +1,65 @@
-import React from 'react'
+import React from "react";
 
-export default function memberForm(props){
+export default function memberForm(props) {
+  const { values, submit, change, disabled, errors } = props;
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
 
-    const{
-        values,
-        submit,
-        change,
-        disabled,
-        errors
-    } = props
-    const onSubmit = evt => {
-        evt.preventDefault()
-        submit()
-    }
-    const onChange = evt => {
-        const {name, value, type, checked} = evt.target
-        const valueToUse = type === 'checkbox' ? checked : value;
-        change(name, valueToUse)
-    }
-    
-    
-    return(
-        <form onSubmit={onSubmit}>
-            <button disabled={disabled}>Submit</button>
+  return (
+    <form onSubmit={onSubmit}>
+      <button disabled={disabled}>Submit</button>
 
-            <div>
-                <div>{errors.password}</div>
-                <div>{errors.email}</div>
-            </div>
-            <label>Username
-                <input 
-                    value={values.username}
-                    onChange={onChange}
-                    name='username'
-                    type='text'
-                />
-            </label>
+      <div>
+        <div>{errors.password}</div>
+        <div>{errors.email}</div>
+      </div>
+      <label>
+        Username
+        <input
+          value={values.username}
+          onChange={onChange}
+          name="username"
+          type="text"
+        />
+      </label>
 
-            <label>Email
-                <input
-                 value={values.email}
-                 onChange={onChange}
-                 name='email'
-                 type='text'
-                />
-            </label>
-            
-            <label>Password
-                <input 
-                value={values.password}
-                onChange={onChange}
-                name='password'
-                type='text'
-                />
-            </label>
+      <label>
+        Email
+        <input
+          value={values.email}
+          onChange={onChange}
+          name="email"
+          type="text"
+        />
+      </label>
 
-            <label>Terms and Conditions
-                <input 
-                type='checkbox'
-                name='terms'
-                value='check'
-                checked={values.terms}
-                onChange={onChange}
-                />
-            </label>
+      <label>
+        Password
+        <input
+          value={values.password}
+          onChange={onChange}
+          name="password"
+          type="text"
+        />
+      </label>
 
-        
-    
-        </form>
-    )
+      <label>
+        Terms and Conditions
+        <input
+          type="checkbox"
+          name="terms"
+          value="check"
+          checked={values.terms}
+          onChange={onChange}
+        />
+      </label>
+    </form>
+  );
 }
-
-
--- advanced/src/Components/Schema.js
+++ advanced/src/Components/Schema.js
@@ -1,20 +1,18 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 export default yup.object().shape({
-    username: yup
+  username: yup
     .string()
-    .required('Username is required')
-    .min(3,'Username must be 3 characters long'),
-    email: yup
+    .required("Username is required")
+    .min(3, "Username must be 3 characters long"),
+  email: yup
     .string()
-    .email('Must be a valid email address')
-    .required('Must include email address'),
-    password: yup
+    .email("Must be a valid email address")
+    .required("Must include email address"),
+  password: yup
     .string()
-    .required('Password is required')
-    .min(8,'Password must be 8 characters long'),
+    .required("Password is required")
+    .min(8, "Password must be 8 characters long"),
 
-    terms: yup
-    .boolean().oneOf([true], 'This is required')
-
-})
+  terms: yup.boolean().oneOf([true], "This is required"),
+});
